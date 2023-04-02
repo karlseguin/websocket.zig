265671aa9886b0ce491f920545cc1467069b1601
Karl Seguin <k@openmymind.io>
2023-04-02 18:13:16 +0800

    use new build.zig changes for tests

--- build.zig
+++ build.zig
@@ -12,12 +12,14 @@ pub fn build(b: *std.Build) !void {
 	});
 	lib.install();
 
-	const tests = b.addTest(.{
+	const lib_test = b.addTest(.{
 		.root_source_file = .{ .path = "src/websocket.zig" },
 		.target = target,
 		.optimize = optimize,
 	});
+	const run_test = b.addRunArtifact(lib_test);
+	run_test.has_side_effects = true;
 
 	const test_step = b.step("test", "Run tests");
-	test_step.dependOn(&tests.step);
+	test_step.dependOn(&run_test.step);
 }
