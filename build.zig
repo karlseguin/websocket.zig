328b8cba932d2a39da1ada6efe6001179a9c1aaa
Karl Seguin <k@openmymind.io>
2024-01-06 09:46:57 +0800

    source_file -> root_source_file for latest breaking zig changes

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
 	const optimize = b.standardOptimizeOption(.{});
 
 	_ = b.addModule("websocket", .{
-		.source_file = .{ .path = "src/websocket.zig" },
+		.root_source_file = .{ .path = "src/websocket.zig" },
 	});
 
 	const lib_test = b.addTest(.{
