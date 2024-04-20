ffd290413804ec75eb063a50b2e0426b5e447910
Fabio Arnold <fabio@fabioarnold.de>
2024-04-20 12:55:17 +0200

    Update build.zig for Zig 0.12.0

--- build.zig
+++ build.zig
@@ -5,16 +5,15 @@ pub fn build(b: *std.Build) !void {
 	const optimize = b.standardOptimizeOption(.{});
 
 	_ = b.addModule("websocket", .{
-		.root_source_file = .{ .path = "src/websocket.zig" },
+		.root_source_file = b.path("src/websocket.zig"),
 	});
 
 	const lib_test = b.addTest(.{
-		.root_source_file = .{ .path = "src/websocket.zig" },
+		.root_source_file = b.path("src/websocket.zig"),
 		.target = target,
 		.optimize = optimize,
-		.test_runner = .{ .path = "test_runner.zig" }
+		.test_runner = b.path("test_runner.zig"),
 	});
-
 	const run_test = b.addRunArtifact(lib_test);
 	run_test.has_side_effects = true;
 
